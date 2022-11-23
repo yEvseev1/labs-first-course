d2819cdf1f1662f893f0e0074ca0d10d82007d14
yEvseev1 <116905207+yEvseev1@users.noreply.github.com>
2022-11-23 23:10:31 +0300

    Add files via upload

---
+++ lab11Remake.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main(){ 
+    setlocale(LC_ALL,"Rus");
+    char a;
+    int flag = 0;
+    int n1 = 0, n0,x;
+    printf("(☞ ͡ ͡° ͜ ʖ ͡ ͡°)☞\n");
+    printf("Введите любые числа\n");
+    while (flag != 404) {
+        a = getchar();
+        if (a != ' ' && a != ',' && a != '\n' && a != '\t') putchar(a);
+        if ( (a < '0'|| a > '9') && (a < 'A' || a > 'F') && (a != ' ' && a != ',' && a != '\n' && a != '\t') ) flag = 1;
+        if (a == ' '|| a == ',' || a == '\n' || a == '\t') {
+            if (flag == 1) {
+                printf(" : не является 16-ичным числом");
+                flag = 0;
+            } else {
+                x = n0;
+                n0 = n1;
+                n1 = 0;
+                flag = 0;
+            }
+            printf("\n");
+        } 
+        if (a == 'A') n1 = n1 * 16 + 10;
+        if (a == 'B') n1 = n1 * 16 + 11;
+        if (a == 'C') n1 = n1 * 16 + 12;
+        if (a == 'D') n1 = n1 * 16 + 13;
+        if (a == 'E') n1 = n1 * 16 + 14;
+        if (a == 'F') n1 = n1 * 16 + 15;
+        if (a <= '9' && a >= '0') n1 = n1 * 16 + (int)(a - '0');
+
+        if (a == EOF) {
+        flag = 404;
+        printf("Предпоследнее 16 - ичное число: %x\n",x);
+        printf("✺◟( ͡° ͜ʖ ͡°)◞✺\n");
+        }
+    }
+}
