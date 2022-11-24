6c93c283cfff27637be92d33bbbe5b1ba7be18af
yEvseev1 <116905207+yEvseev1@users.noreply.github.com>
2022-11-24 19:53:23 +0300

    Add files via upload

--- lab11Remake.c
+++ lab11Remake.c
@@ -5,25 +5,21 @@ int main(){
     setlocale(LC_ALL,"Rus");
     char a;
     int flag = 0;
-    int n1 = 0, n0,x;
-    printf("(☞ ͡ ͡° ͜ ʖ ͡ ͡°)☞\n");
+    int n1 = 0, n0, x, flag1 = 0;
     printf("Введите любые числа\n");
-    while (flag != 404) {
+    while (a != '\n') {
         a = getchar();
-        if (a != ' ' && a != ',' && a != '\n' && a != '\t') putchar(a);
         if ( (a < '0'|| a > '9') && (a < 'A' || a > 'F') && (a != ' ' && a != ',' && a != '\n' && a != '\t') ) flag = 1;
         if (a == ' '|| a == ',' || a == '\n' || a == '\t') {
-            if (flag == 1) {
-                printf(" : не является 16-ичным числом");
-                flag = 0;
-            } else {
+            if (flag == 0) {
                 x = n0;
                 n0 = n1;
                 n1 = 0;
+                flag1 += 1;
+            } else {
                 flag = 0;
             }
-            printf("\n");
-        } 
+        }
         if (a == 'A') n1 = n1 * 16 + 10;
         if (a == 'B') n1 = n1 * 16 + 11;
         if (a == 'C') n1 = n1 * 16 + 12;
@@ -31,11 +27,11 @@ int main(){
         if (a == 'E') n1 = n1 * 16 + 14;
         if (a == 'F') n1 = n1 * 16 + 15;
         if (a <= '9' && a >= '0') n1 = n1 * 16 + (int)(a - '0');
-
-        if (a == EOF) {
-        flag = 404;
+        if (a == '\n' && flag1 > 1) {
         printf("Предпоследнее 16 - ичное число: %x\n",x);
-        printf("✺◟( ͡° ͜ʖ ͡°)◞✺\n");
-        }
+            }
+        if (a == '\n' && flag1 < 2) {
+        printf("Одно число(Отсутствие чисел).\n");
+            }
     }
 }
