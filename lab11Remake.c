2902e090331d7c72083d296ad850facf027b5a4a
yEvseev1 <116905207+yEvseev1@users.noreply.github.com>
2022-11-26 02:42:16 +0300

    Add files via upload

--- lab11Remake.c
+++ lab11Remake.c
@@ -5,17 +5,20 @@ int main(){
     setlocale(LC_ALL,"Rus");
     char a = '0';
     int flag = 0;
-    int n1 = 0, n0, x, flag1 = 0;
+    int n1 = 0, n0, x, flag1 = 0, flag2 = 1;
     printf("Введите любые числа\n");
-    while (a != '\n') {
+    while (a != EOF) {
         a = getchar();
-        if ( (a < '0'|| a > '9') && (a < 'A' || a > 'F') && (a != ' ' && a != ',' && a != '\n' && a != '\t') ) flag = 1;
+        if ( (a < '0'|| a > '9') && (a < 'A' || a > 'F') && (a != ' ' && a != ',' && a != '\n' && a != '\t' && a != '-') ) flag = 1;
+        if (a == '-') flag2 = -1;
         if (a == ' '|| a == ',' || a == '\n' || a == '\t') {
             if (flag == 0) {
                 x = n0;
-                n0 = n1;
+                if (n1 != 0) n0 = n1 * flag2;
                 n1 = 0;
                 flag1 += 1;
+                flag2 = 1;
+                //flag2 = 0;
             } else {
                 flag = 0;
             }
@@ -27,11 +30,15 @@ int main(){
         if (a == 'E') n1 = n1 * 16 + 14;
         if (a == 'F') n1 = n1 * 16 + 15;
         if (a <= '9' && a >= '0') n1 = n1 * 16 + (int)(a - '0');
-        if (a == '\n' && flag1 > 1) {
-        printf("Предпоследнее 16 - ичное число: %x\n",x);
+        if (a == EOF && flag1 > 1) {
+            if(x >= 0){
+                printf("Предпоследнее 16 - ичное число: %x\n",x);
+            } else {
+                printf("Предпоследнее 16 - ичное число: -%x\n",(0-x));
             }
-        if (a == '\n' && flag1 < 2) {
+            }
+        if (a == EOF && flag1 < 2) {
         printf("Одно число(Отсутствие чисел).\n");
-            }
+            } 
     }
 }
