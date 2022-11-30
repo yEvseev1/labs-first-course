f0147846426f1c8096b72c3e5d05dc4030d39880
yEvseev1 <116905207+yEvseev1@users.noreply.github.com>
2022-11-30 22:07:15 +0300

    Add files via upload

--- lab11Remake.c
+++ lab11Remake.c
@@ -3,42 +3,53 @@
 
 int main(){ 
     setlocale(LC_ALL,"Rus");
-    char a = '0';
+    char a1 = '0';
+    char a0 = 'g';
     int flag = 0;
-    int n1 = 0, n0, x, flag1 = 0, flag2 = 1;
+    int n1 = 0, n0, x, flag1 = 0, flag2 = 1, flag3 = 0;
     printf("Введите любые числа\n");
-    while (a != EOF) {
-        a = getchar();
-        if ( (a < '0'|| a > '9') && (a < 'A' || a > 'F') && (a != ' ' && a != ',' && a != '\n' && a != '\t' && a != '-') ) flag = 1;
-        if (a == '-') flag2 = -1;
-        if (a == ' '|| a == ',' || a == '\n' || a == '\t') {
+    while (a1 != EOF) {
+        a1 = getchar();
+        if ((a0 <= '9' && a0 >= '0') || (a0 >= 'A' && a0 <= 'F')) flag3 = 1;
+        if ( (a1 < '0'|| a1 > '9') && (a1 < 'A' || a1 > 'F') && (a1 != ' ' && a1 != ',' && a1 != '\n' && a1 != '\t' && a1 != '-') ) flag = 1;
+        if (a1 == '-'){
+            if ((a0 <= '9' && a0 >= '0') || (a0 >= 'A' && a0 <= 'F') ){
+                flag = 1;
+            } else {
+                flag2 = -1;
+            }
+        }
+        if (a1 == ' '|| a1 == ',' || a1 == '\n' || a1 == '\t' || a1 == EOF) {
             if (flag == 0) {
                 x = n0;
-                if (n1 != 0) n0 = n1 * flag2;
+                if (flag3 == 1) {
+                    n0 = n1 * flag2;
+                    flag3 = 0;
+                }
                 n1 = 0;
                 flag1 += 1;
                 flag2 = 1;
-                //flag2 = 0;
             } else {
                 flag = 0;
             }
         }
-        if (a == 'A') n1 = n1 * 16 + 10;
-        if (a == 'B') n1 = n1 * 16 + 11;
-        if (a == 'C') n1 = n1 * 16 + 12;
-        if (a == 'D') n1 = n1 * 16 + 13;
-        if (a == 'E') n1 = n1 * 16 + 14;
-        if (a == 'F') n1 = n1 * 16 + 15;
-        if (a <= '9' && a >= '0') n1 = n1 * 16 + (int)(a - '0');
-        if (a == EOF && flag1 > 1) {
+        if (a1 == 'A') n1 = n1 * 16 + 10;
+        if (a1 == 'B') n1 = n1 * 16 + 11;
+        if (a1 == 'C') n1 = n1 * 16 + 12;
+        if (a1 == 'D') n1 = n1 * 16 + 13;
+        if (a1 == 'E') n1 = n1 * 16 + 14;
+        if (a1 == 'F') n1 = n1 * 16 + 15;
+        if (a1 <= '9' && a1 >= '0') n1 = n1 * 16 + (int)(a1 - '0');
+        if (a1 == EOF && flag1 > 1) {
             if(x >= 0){
                 printf("Предпоследнее 16 - ичное число: %x\n",x);
             } else {
                 printf("Предпоследнее 16 - ичное число: -%x\n",(0-x));
             }
             }
-        if (a == EOF && flag1 < 2) {
+        if (a1 == EOF && flag1 < 2) {
         printf("Одно число(Отсутствие чисел).\n");
             } 
+        a0 = a1;
     }
 }
