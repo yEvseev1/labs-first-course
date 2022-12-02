37bf023cd3cf71bda3ea73e9db47c13ad08325f9
yEvseev1 <116905207+yEvseev1@users.noreply.github.com>
2022-12-02 20:09:19 +0300

    Add files via upload

---
+++ 13lab.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <locale.h>
+
+#define DEAF (1ull << ('q' - 'a') | 1ull << ('t' - 'a') | 1ull << ('p' - 'a') | 1ull << ('s' - 'a') | 1ull << ('f' - 'a') | 1ull << ('h' - 'a') | 1ull << ('k' - 'a') | 1ull << ('x' - 'a') | 1ull << ('c' - 'a'))
+// #define DEAF 76383396ULL работает одинаково ,если по букве, или если перевести двоичный код в 10, где 1-цы это глухие согласные
+#define VOWELS 17842449ULL
+
+unsigned long long int CharInSet(int a) {
+    if (a >= 'a' && a <= 'z') {
+        return 1ull << (a - 'a');
+    } else {
+        return 0;
+    }
+}
+
+int main() {
+    char a;
+    unsigned long long int Set = 0 , NewSet = 0;
+    a = getchar();
+    while (a != EOF) {
+        if (a != ' ' && a != '\n') {
+            Set = Set | CharInSet(a);
+        } else {
+            NewSet = Set & (DEAF | VOWELS);
+            if (NewSet == Set) break;
+            Set = 0;
+        }
+        a = getchar();
+    }
+    NewSet = Set & (DEAF | VOWELS);
+    if ((NewSet == Set) && Set != 0) {
+        printf("Есть слово,все буквы которого глухие согласные!\n");
+    } else {
+        printf("Нет такого слова.\n");
+    }
+}
